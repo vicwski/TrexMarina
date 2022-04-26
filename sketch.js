5d701243133d479140a7bc93917049ef33f730fa
vicwski <vic.gajewski@gmail.com>
2022-04-26 17:59:38 -0300

    skecth update aula 14

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var cloudImg
 var cloudsGroup, obstaclesGroup
 var gameOver, gameOverImg
 var restart, restartImg
-
+var score = 0
 
 function preload() {
   trex_running = loadAnimation(
@@ -30,6 +30,8 @@ function preload() {
   obstacle5 = loadImage('sprites/obstacle5.png')
   obstacle6 = loadImage('sprites/obstacle6.png')
 
+  gameOverImg = loadImage('sprites/gameOver.png')
+  restartImg = loadImage('sprites/restart.png')
 }
 
 function setup() {
@@ -50,6 +52,15 @@ function setup() {
   invisibleGround = createSprite(200, 190, 400, 10)
   invisibleGround.visible = false
 
+  //sprite Game Over
+  gameOver = createSprite(300, 80)
+  gameOver.addImage(gameOverImg)
+
+  //sprite botão de restart
+  restart = createSprite(300, 130)
+  restart.addImage(restartImg)
+  restart.scale = 0.5
+
   //criar grupos
   cloudsGroup = new Group()
   obstaclesGroup = new Group()
@@ -57,9 +68,14 @@ function setup() {
 
 function draw() {
   //definir cor do plano de fundo
-  background(120)
+  background(230)
 
-  if (gameState == PLAY) {
+  if (gameState === PLAY) {
+    //invisivilidade das sprites gameOver e restart
+    gameOver.visible = false
+    restart.visible = false
+
+    //Reiniciar o solo
     if (ground.x < 0) {
       ground.x = ground.width / 2
     }
@@ -77,18 +93,31 @@ function draw() {
     //chamar função de criar os obstaculos
     criarObstacles()
 
+    //condição para alterar o estado do jogo
     if (obstaclesGroup.isTouching(trex)) {
       gameState = END
     }
-  } else if (gameState == END) {
-  
-  
+
+  } else if (gameState === END) {
+    //visivilidade das sprites gameOver e restart
+    gameOver.visible = true
+    restart.visible = true
+
+    //Parando o solo e os grupos de sprite quando o estado do jogo é END
+    ground.velocityX = 0
+    obstaclesGroup.setVelocityXEach(0)
+    cloudsGroup.setVelocityXEach(0)
   }
 
   //impedir que o trex caia
   trex.collide(invisibleGround)
 
   drawSprites()
+
+  //texto da pontuação
+  stroke(100)
+  fill(120)
+  text('Score: ' + score, 500, 40)
 }
 
 function gravity() {
